87c13f5d17d336a23bd61004c4b5849e93437404
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:15:16 +0200

    add app

---
+++ MethodOverridingDemo/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MethodOverridingDemo
+{
+    public abstract class Car
+    {
+        public virtual void StartCar()
+        {
+            Console.WriteLine("Turn key and start");
+        }
+
+        public abstract void SetClock();
+    }
+}
